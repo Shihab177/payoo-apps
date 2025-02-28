524be07d023cc29894013bb44ab13b58a2f444e1
Shihab177 <shihab7777h@gmail.com>
2025-02-27 22:33:08 -0800

    solved

--- main.js
+++ main.js
@@ -8,12 +8,27 @@ document.getElementById('addMoney-btn').addEventListener('click',function(event)
     const convertAmount =parseFloat(amount)
     const mainBalance= document.getElementById('main-balance').innerText;
     const convertMainBalance = parseFloat(mainBalance)
-
-    if (accountNumber.length ===11){
+    const selectBank = document.getElementById('select-bank').value;
+    console.log(selectBank)
+     if(convertAmount < 0){
+      alert('invalid amounts');
+      return
+     }
+    if (accountNumber.length ===11 && selectBank){
      if (convertPin=== 1234){
        if (convertAmount){
         const sum =convertMainBalance + convertAmount;
         document.getElementById('main-balance').innerText=sum;
+        const commentDiv = document.getElementById('comment-div');
+        const div = document.createElement('div');
+        div.classList.add('border','border-red-500','mb-2', 'rounded-[15px]' ,'py-3','px-2')
+        div.innerHTML= `
+        <h3>from ${selectBank}</h3>
+        <h1 class="text-[16px] font-bold">Add Money</h1>
+        <p class="text-[16px] font-bold">${convertAmount}</p>
+        <p class="text-[16px] font-bold">account number: ${accountNumber}</p>
+        `
+        commentDiv.appendChild(div)
        }
      }
      else{
--- toggle.js
+++ toggle.js
@@ -1,11 +1,19 @@
 document.getElementById('cash-out').style.display='none'
+document.getElementById('history').style.display='none';
+
 document.getElementById('add-money-box').addEventListener('click',function(){
     document.getElementById('cash-out').style.display='none'
     document.getElementById('add-money').style.display='block'
+    document.getElementById('history').style.display='none';
      
 })
 document.getElementById('cash-out-box').addEventListener('click',function(){
     document.getElementById('cash-out').style.display='block'
     document.getElementById('add-money').style.display='none'
-     
+    document.getElementById('history').style.display='none';
+})
+document.getElementById('Transactions-box').addEventListener('click',function(){
+    document.getElementById('history').style.display='block';
+    document.getElementById('add-money').style.display='none';
+    document.getElementById('cash-out').style.display='none'
 })
